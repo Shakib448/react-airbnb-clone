de2ee4036b258ea6c5358837cec4c757825e4961
Shakib448 <shakiba448@gmail.com>
2020-09-07 18:50:21 +0600

    React Airbnb Clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from './Components/SearchPage/SearchPage';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Router>
+        <Header />
+        <Switch>
+          <Route  exact path='/' component={Home}/>
+          <Route  exact path='/search' component={SearchPage} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='footer'>
+                <p>© 2020 Airbnb clone! No rights reserved - this is a demo! Made by Muktadir</p>
+                <p>Privacy · Terms · Sitemap · Company Details</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
